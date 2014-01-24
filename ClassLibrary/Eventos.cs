8d07361cfd6bdc80a5430a85b56c11463752fc32
Uge Hidalgo <ugehidalgo@hotmail.com>
2014-01-24 08:21:37 +0100

    Event example started

---
+++ ClassLibrary/Eventos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    class Eventos
+    {
+        private string txt;
+        public event EventHandler ok;
+
+        public Eventos(string texto)
+        {
+            this.txt = texto;
+        }
+
+        public void Escribir()
+        {
+            Console.WriteLine(txt);
+            ok ("Evento capturado",null);
+        }
+    }
+}
